42bd122727e3eb9e3801e444e8b833a87ec4053e
Specifix <62927726+Specifix5@users.noreply.github.com>
2024-07-31 20:13:16 +0800

    Update chat.lua

--- chat.lua
+++ chat.lua
@@ -1,8 +1,31 @@
 local cb = peripheral.find("chatBox")
 
-while os.sleep(0.25) do
+function string.starts(String,Start)
+    return string.sub(String,1,string.len(Start))==Start
+end
+
+function string.split(inputstr, sep)
+    if sep == nil then
+      sep = "%s"
+    end
+    local t = {}
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+      table.insert(t, str)
+    end
+    return t
+end
+
+local messageHistory = {}
+
+while os.sleep(0.1) do
     local event, username, message, uuid, isHidden = os.pullEvent("chat")
 
-    
-    cb.sendMessage(message:gsub("o", "wo"):gsub("r", "w"), username, "<>");
+    messageHistory[username] = message
+
+    if string.starts(username, "nekoyuri") then
+        local args = string.split(message, " ")
+        if string.starts(message, "femboy") then
+            cb.sendMessage(messageHistory[args[2]]:gsub("o", "wo"):gsub("r", "w").." >w<", username, "<>");
+        end
+    end
 end
